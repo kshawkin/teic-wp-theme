37c6ba2dc87f3ee507c751f61c34524aad587256
Michael Schofield <schoeyfield@Users-MacBook-Air.local>
2016-09-04 00:37:14 -0400

    Removes table of contents from home and search pages.

--- sidebar.php
+++ sidebar.php
@@ -18,63 +18,65 @@ if ( ! is_active_sidebar( 'sidebar-1' ) ) {
 	<?php //dynamic_sidebar( 'sidebar-1' ); ?>
 
 	<?php
-	global $post;
+	if ( !is_search() && !is_page( 'home' ) ) :
+		global $post;
+		$page = get_the_ID();
 
-	$page = get_the_ID();
+		if ( $post->post_parent > 0 ) {
 
-	if ( $post->post_parent > 0 ) {
+			/* Get an array of Ancestors and Parents if they exist */
+			$parents = get_post_ancestors( $post->ID );
 
-		/* Get an array of Ancestors and Parents if they exist */
-		$parents = get_post_ancestors( $post->ID );
+			/* Get the top Level page->ID count base 1, array base 0 so -1 */
+			$id = ($parents) ? $parents[count($parents)-1]: $post->ID;
 
-		/* Get the top Level page->ID count base 1, array base 0 so -1 */
-		$id = ($parents) ? $parents[count($parents)-1]: $post->ID;
+			/* Get the parent and set the $class with the page slug (post_name) */
+			$parent = get_post( $id );
+			$page = $parent->ID;
+		}
 
-		/* Get the parent and set the $class with the page slug (post_name) */
-		$parent = get_post( $id );
-		$page = $parent->ID;
-	}
-
-	/**
-	 * Return a list of sub-pages
-	 */
-	$args = array(
-		'child_of' => $page,
-		'depth' => 1,
-		'echo' => false,
-		'title_li' => ''
-	); ?>
-
-	<ul>
-	<?php echo wp_list_pages( $args );
-
-	/**
-	 * If a child page, print the title as an h2
-	 */
-	if ( $post->post_parent > 0 ) : ?>
-		<?php if ( count( get_pages( 'child_of=' . get_the_ID() ) ) > 0 ) : ?>
-
-		<ul class="sub-menu">
-		<?php echo wp_list_pages( array(
-			'child_of' => get_the_ID(),
-			'depth' => 1,
-			'title_li' => '',
-			'echo' => false
-			));	?>
-
-		<?php elseif ( ( $post->post_parent !== $parent->ID ) && ( count( get_pages( 'child_of=' . wp_get_post_parent_id( get_the_ID() ) ) ) > 0 ) ) : ?>
-
-		<ul class="sub-menu sub-menu__links">
-		<?php echo wp_list_pages( array(
-			'child_of' => wp_get_post_parent_id( get_the_ID() ),
+		/**
+		 * Return a list of sub-pages
+		 */
+		$args = array(
+			'child_of' => $page,
 			'depth' => 1,
 			'echo' => false,
 			'title_li' => ''
-			));
-		?>
-		</ul>
-		<?php endif; ?>
-  <?php endif; ?>
+		); ?>
+
+		<ul>
+		<?php echo wp_list_pages( $args );
+
+		/**
+		 * If a child page, print the title as an h2
+		 */
+		if ( $post->post_parent > 0 ) : ?>
+			<?php if ( count( get_pages( 'child_of=' . get_the_ID() ) ) > 0 ) : ?>
+
+			<ul class="sub-menu">
+			<?php echo wp_list_pages( array(
+				'child_of' => get_the_ID(),
+				'depth' => 1,
+				'title_li' => '',
+				'echo' => false
+				));	?>
+
+			<?php elseif ( ( $post->post_parent !== $parent->ID ) && ( count( get_pages( 'child_of=' . wp_get_post_parent_id( get_the_ID() ) ) ) > 0 ) ) : ?>
+
+			<ul class="sub-menu sub-menu__links">
+			<?php echo wp_list_pages( array(
+				'child_of' => wp_get_post_parent_id( get_the_ID() ),
+				'depth' => 1,
+				'echo' => false,
+				'title_li' => ''
+				));
+			?>
+			</ul>
+			<?php endif; ?>
+	  <?php endif; ?>
+
+	<?php endif; ?>
 
 	</div>
 </aside><!-- #secondary -->
